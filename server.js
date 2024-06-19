ec47e926064f2ad3ce52deb2bcfdda7abffc3d27
Benoît Vandanjon <benoit@vandanjon.fr>
2024-06-19 17:17:50 +0200

    test base routes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const routes = require("./routes");
 const usersRoutes = require("./routes/usersRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/", routes);
 app.use("/users", usersRoutes);
 
 const PORT = process.env.PORT || 3200;
